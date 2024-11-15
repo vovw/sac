863a6a5f2bd06b0e46378e02ae0b49680058ca02
crisiumnih <fasfasmag@proton.me>
2024-11-15 13:14:46 +0530

    worksssss

--- main/line_following.c
+++ main/line_following.c
@@ -5,13 +5,26 @@
 #include "tuning_http_server.h"
 #include "ir.h"
 
+// Add these definitions at the top
+#define ALL_BLACK_THRESHOLD 1000  // Threshold for detecting all black
+#define ALL_WHITE_THRESHOLD 100  // Threshold for detecting all white
+#define INTERSECTION_SENSOR_COUNT 3  // Minimum number of sensors needed to detect intersection
+
+// Add these function declarations
+bool is_intersection();
+bool can_turn_left();
+bool can_go_straight();
+bool is_all_black();
+bool should_turn_180();
+
+
 #define MODE NORMAL_MODE
 #define BLACK_MARGIN 4095
 #define WHITE_MARGIN 0
 #define bound_LSA_LOW 0
 #define bound_LSA_HIGH 1000
 #define BLACK_BOUNDARY  930    // Boundary value to distinguish between black and white readings
-#define ROTATION_SPEED 60 
+#define ROTATION_SPEED 65
 int all_black_counter = 0;
 
 /*
@@ -23,7 +36,7 @@ const int weights[5] = {-5, -3, 1, 3, 5};
  * Motor value boundts
  */
 int optimum_duty_cycle = 60;
-int lower_duty_cycle = 5;
+int lower_duty_cycle = 45;
 extern int higher_duty_cycle;
 
 int higher_duty_cycle = 67;
@@ -58,7 +71,7 @@ void calculate_correction()
     difference = error - prev_error;
     cumulative_error += error;
 
-    cumulative_error = bound(cumulative_error, -30, 30);
+    cumulative_error = bound(cumulative_error, -40, 40);
 
     correction = read_pid_const().kp*error + read_pid_const().ki*cumulative_error + read_pid_const().kd*difference;
     prev_error = error;
@@ -94,7 +107,7 @@ void calculate_error()
     }
 
     if(all_black_flag == 1)
-    {	
+    {
 	all_black_counter++;
 	if(all_black_counter>=8){
 		error = 999;
@@ -113,16 +126,56 @@ void calculate_error()
     }
     else
     {
-	all_black_counter = 0; 
+	all_black_counter = 0;
         error = pos;
     }
 }
 
 
 
+bool is_intersection() {
+    int black_count = 0;
+    for(int i = 0; i < 5; i++) {
+        if(line_sensor_readings.adc_reading[i] > BLACK_BOUNDARY) {
+            black_count++;
+        }
+    }
+    return black_count >= INTERSECTION_SENSOR_COUNT;
+}
 
+bool can_turn_left() {
+    // Check if leftmost sensor detects black
+    return line_sensor_readings.adc_reading[0] > BLACK_BOUNDARY;
+}
+
+bool can_go_straight() {
+    // Check if center sensor detects black
+    return line_sensor_readings.adc_reading[2] > BLACK_BOUNDARY;
+}
+
+bool is_all_black() {
+    for(int i = 0; i < 5; i++) {
+        if(line_sensor_readings.adc_reading[i] < BLACK_BOUNDARY) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool should_turn_180() {
+    // Check if all sensors detect white (dead end)
+    for(int i = 0; i < 5; i++) {
+        if(line_sensor_readings.adc_reading[i] > ALL_WHITE_THRESHOLD) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Modify the line_follow_task function
 void line_follow_task(void* arg)
 {
+    // ... (keep existing initialization code)
     motor_handle_t motor_a_0, motor_a_1;
     ESP_ERROR_CHECK(enable_motor_driver(&motor_a_0, MOTOR_A_0));
     ESP_ERROR_CHECK(enable_motor_driver(&motor_a_1, MOTOR_A_1));
@@ -139,22 +192,26 @@ void line_follow_task(void* arg)
 
 #endif
 
+
     while(true)
     {
-	line_sensor_readings = read_line_sensor(line_sensor);
-	for(int i = 0; i < 5; i++)
-	{
-	    line_sensor_readings.adc_reading[i] = bound(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN);
-	    line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN, bound_LSA_LOW, bound_LSA_HIGH);
-	    line_sensor_readings.adc_reading[i] = 1000 - (line_sensor_readings.adc_reading[i]);
-	}
+        line_sensor_readings = read_line_sensor(line_sensor);
+        for(int i = 0; i < 5; i++)
+        {
+            line_sensor_readings.adc_reading[i] = bound(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN);
+            line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN, bound_LSA_LOW, bound_LSA_HIGH);
+            line_sensor_readings.adc_reading[i] = 1000 - (line_sensor_readings.adc_reading[i]);
+        }
 
-	calculate_error();
-	if (error == 999) {
-            // Rotate in place until white line is detected
+        if(should_turn_180()) {
+            // Perform 180-degree turn
+            //
+            calculate_error();
+            calculate_correction();
+
             set_motor_speed(motor_a_0, MOTOR_FORWARD, ROTATION_SPEED);
             set_motor_speed(motor_a_1, MOTOR_BACKWARD, ROTATION_SPEED);
-            
+
             // Continue rotating until white line is detected
             while (!is_white_detected()) {
                 line_sensor_readings = read_line_sensor(line_sensor);
@@ -165,42 +222,51 @@ void line_follow_task(void* arg)
                 }
                 vTaskDelay(10 / portTICK_PERIOD_MS);
             }
-	    all_black_counter = 0;
-        } else {
-            // Normal line following behavior
-            calculate_correction();
-	if (correction > 0) {  // Need to turn right
-			left_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
-			right_duty_cycle = bound(correction, lower_duty_cycle, higher_duty_cycle);
-			set_motor_speed(motor_a_0, MOTOR_FORWARD, left_duty_cycle);
-			set_motor_speed(motor_a_1, MOTOR_BACKWARD, right_duty_cycle);
-		    } else if (correction < 0) {  // Need to turn left
-			left_duty_cycle = bound(-correction, lower_duty_cycle, higher_duty_cycle);
-			right_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
-			set_motor_speed(motor_a_0, MOTOR_BACKWARD, left_duty_cycle);
-			set_motor_speed(motor_a_1, MOTOR_FORWARD, right_duty_cycle);
-		    } else {  // Go straight
-			left_duty_cycle = right_duty_cycle = optimum_duty_cycle;
-			set_motor_speed(motor_a_0, MOTOR_FORWARD, left_duty_cycle);
-			set_motor_speed(motor_a_1, MOTOR_FORWARD, right_duty_cycle);
-		    }
         }
-	//ESP_LOGI("debug","left_duty_cycle:  %f    ::  right_duty_cycle :  %f  :: error :  %f  correction  :  %f  \n",left_duty_cycle, right_duty_cycle, error, correction);
-	ESP_LOGI("debug", "KP: %f ::  KI: %f  :: KD: %f", read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
-	ESP_LOGI("IR_TEST","IR_VALUE : %d",read_ir());
-#ifdef CONFIG_ENABLE_OLED
-	// Diplaying kp, ki, kd values on OLED
-	if (read_pid_const().val_changed)
-	{
-	    display_pid_values(read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
-	    reset_val_changed_pid_const();
-	}
-#endif
+        else if(is_intersection()) {
+            calculate_error();
+            calculate_correction();
 
-	vTaskDelay(10 / portTICK_PERIOD_MS);
+            if(can_turn_left()) {
+                // Turn left
+                set_motor_speed(motor_a_0, MOTOR_BACKWARD, ROTATION_SPEED);
+                set_motor_speed(motor_a_1, MOTOR_FORWARD, ROTATION_SPEED);
+            }
+            else if(can_go_straight()) {
+                // Go straight
+                set_motor_speed(motor_a_0, MOTOR_FORWARD, optimum_duty_cycle);
+                set_motor_speed(motor_a_1, MOTOR_FORWARD, optimum_duty_cycle);
+            }
+            else {
+                // Turn right
+                set_motor_speed(motor_a_0, MOTOR_FORWARD, ROTATION_SPEED);
+                set_motor_speed(motor_a_1, MOTOR_BACKWARD, ROTATION_SPEED);
+            }
+        }
+        else {
+            // Normal line following behavior
+            calculate_error();
+            calculate_correction();
+
+            if (correction > 0) {
+                left_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
+                right_duty_cycle = bound(correction, lower_duty_cycle, higher_duty_cycle);
+                set_motor_speed(motor_a_0, MOTOR_FORWARD, left_duty_cycle);
+                set_motor_speed(motor_a_1, MOTOR_BACKWARD, right_duty_cycle);
+            } else if (correction < 0) {
+                left_duty_cycle = bound(-correction, lower_duty_cycle, higher_duty_cycle);
+                right_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
+                set_motor_speed(motor_a_0, MOTOR_BACKWARD, left_duty_cycle);
+                set_motor_speed(motor_a_1, MOTOR_FORWARD, right_duty_cycle);
+            } else {
+                left_duty_cycle = right_duty_cycle = optimum_duty_cycle;
+                set_motor_speed(motor_a_0, MOTOR_FORWARD, left_duty_cycle);
+                set_motor_speed(motor_a_1, MOTOR_FORWARD, right_duty_cycle);
+            }
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
-
-    vTaskDelete(NULL);
 }
 
 void app_main()
